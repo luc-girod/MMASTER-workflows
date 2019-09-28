fix strip centroid calculation around the dateline

bin/sort_l1a.py
@@ -206,7 +206,7 @@ def sort_l1a_by_region(list_pulldir, list_shp, out_dir):
             if min_lon < -160 and max_lon > 160:
                 # dateline exception...
 
-                # let's do two full polygons from each side of the dateline...
+                # we need the intersection, so let's do two split polygons on each side of the dateline...
                 lon_rightside = np.array(lon_tup, dtype=float)
                 lon_rightside[lon_rightside < -160] += 360
 
@@ -450,31 +450,18 @@ def sort_strip_by_utm(dir_l1a):
             max_lon = np.max(lon_tup)
             min_lon = np.min(lon_tup)
 
-            if min_lon < -160 and max_lon > 160:
+            if min_lon < -160:
                 # if this is happening, ladies and gentlemen, bad news, we definitely have an image on the dateline
 
-                # let's do two full polygons from each side of the dateline...
+                # here we need the right centroid, so let's do polygons on only one side of the dateline: the right one
                 lon_rightside = np.array(lon_tup, dtype=float)
                 lon_rightside[lon_rightside < -160] += 360
-
-                lon_leftside = np.array(lon_tup, dtype=float)
-                lon_leftside[lon_leftside > 160] -= 360
 
                 rightside_coord = list(zip(list(lon_rightside) + [lon_rightside[0]], lat_tup + [lat_tup[0]]))
                 rightside_poly = poly_from_coords(rightside_coord)
 
-                leftside_coord = list(zip(list(lon_leftside) + [lon_leftside[0]], lat_tup + [lat_tup[0]]))
-                leftside_poly = poly_from_coords(leftside_coord)
-
-                # create a world polygon and get intersection
-                world_coord = [(-180, -90), (-180, 90), (180, 90), (180, -90), (-180, -90)]
-                world_poly = poly_from_coords(world_coord)
-
-                leftside_inters = world_poly.Intersection(leftside_poly)
-                rightside_inters = world_poly.Intersection(rightside_poly)
-
-                # add both to list
-                list_poly += [leftside_inters, rightside_inters]
+                # add to list
+                list_poly.append(rightside_poly)
             else:
                 list_coord = list(zip(lon_tup + [lon_tup[0]], lat_tup + [lat_tup[0]]))
                 poly = poly_from_coords(list_coord)
@@ -484,7 +471,11 @@ def sort_strip_by_utm(dir_l1a):
         union = union_cascaded_multipoly(multipoly)
         centroid = get_poly_centroid(union)
 
-        _, utm = latlon_to_UTM(centroid[1], centroid[0])
+        #now we get a centroid with longitude in a -180/180 longitude
+
+        final_lon = ((centroid[0] + 180) % 360 ) - 180
+
+        _, utm = latlon_to_UTM(centroid[1], final_lon)
 
         print('Centroid of strip is ' + str(centroid[0]) + ',' + str(
             centroid[1]) + ' [lon/lat] corresponding to UTM zone ' + utm)